64d1829d026abb6d24c415944c88b8f1256841db
KoRaghav <106992952+KoRaghav@users.noreply.github.com>
2023-02-23 20:53:19 +0530

    preloader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Tilt from 'react-parallax-tilt';
-import Registration from './components/Registeration/Registration';
+import React from 'react';
+import Tilt from 'react-parallax-tilt'
+
 import EventDescription from './components/EventDescription/EventDescription'
+import Registration from './components/Registeration/Registration'
+import preloader from './assets/images/preloader.gif';
+
 import './App.css'
 
 function App() {
   return (
     <div>
+
+      <div class="preloader"> <img src={preloader} alt="Image"/>
+        <ul class="text-rotater">
+          <li>Hangin there</li>
+          <li>Still loading</li>
+          <li>Almost done</li>
+        </ul>
+      </div>
+
         <div className='reg'>
           <Registration/>
         </div>

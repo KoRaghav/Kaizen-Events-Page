e29a099115a16ad6ed27ee1ef2710d47753cf3e8
KoRaghav <106992952+KoRaghav@users.noreply.github.com>
2023-02-25 01:14:17 +0530

    event links and images

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import preloader from './assets/images/preloader.gif';
 import './App.css'
 
 import KaizenForm from './components/Form/KaizenForm';
-import { BrowserRouter as Router, Routes, Route,useLocation  } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,useLocation,useNavigate,useParams  } from "react-router-dom";
 import Alert from './components/Alert/alert';
-
+import { events } from './components/events';
 
 function Home() {
   const {state} = useLocation();
@@ -21,12 +21,19 @@ function Home() {
   if (state) {alert = state.alert;}
   else {alert = false;}
   
+  let navigate = useNavigate(); 
+  const event = ({title}) =>{ 
+    let path = `/`; 
+    navigate(path,{ state: { title:title} });
+  }
+
   return (
     <>
     {alert ? <Alert/> : <></>}
       <div class="head pt-5">
         <h2>SOCIOPRENEURS</h2>
         <h3>A platform to motivate aspiring social entrepreneurs to develop their ideas and inspire them for social change</h3>
+
       </div>
       <ul class="background">
         <li></li>
@@ -40,10 +47,8 @@ function Home() {
         <li></li>
         <li></li>
       </ul>
-    <main style={{"position":"relative","bottom":"-40vh"}}>
-      <div className='reg p-3'>
-        <Registration />
-      </div>
+    <main style={{"position":"relative","top":"15vh"}}>
+
 
       <div className='reg'>
         <Tilt className="each-event" style={{ backgroundColor: "transparent" }}>
@@ -55,6 +60,50 @@ function Home() {
     </>
   )
 }
+
+function Event() {
+  const { id } = useParams();
+
+  const title = events[0][`${id}`]["title"];
+  const description = events[0][`${id}`]["description"];
+  const image = events[0][`${id}`]["image"];
+  console.log(image);
+  return (
+    <>
+      <div class="head pt-5">
+        <h2>{title}</h2>
+        <h3>{description}</h3>
+      </div>
+      <ul class="background">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    <main style={{"position":"relative","top":"15vh"}}>
+      {/* <div className='reg p-3'>
+        <Registration />
+      </div> */}
+
+      <img src={image}/>
+
+      <div className='reg'>
+        <Tilt className="each-event" style={{ backgroundColor: "transparent" }}>
+          <EventDescription />
+        </Tilt>
+      </div>
+      <div style={{minHeight:"100vh"}}></div>
+    </main>
+    </>
+  )
+}
+
 
 function App() {
 
@@ -77,6 +126,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/form" element={<KaizenForm />} />
+          <Route path=":id" element={<Event/>} />
         </Routes>
       </Router>
 
